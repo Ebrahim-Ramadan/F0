e1b2b177b597925ebcd0e64454800adf1270aa5c
Ebrahim-Ramadan <ramadanebrahim791@gmail.com>
2024-09-14 03:40:42 +0200

    fixed the server-side redirect after login on /api/addUser

--- app/api/addUser/route.ts
+++ app/api/addUser/route.ts
@@ -5,6 +5,7 @@ import { revalidatePath } from "next/cache";
 import { NextResponse } from "next/server";
 
 export async function POST(req:Request) {
+  const url = new URL(req.url);
   try {
     const { username, password, pic } = await req.json();
 
@@ -28,7 +29,9 @@ export async function POST(req:Request) {
     await createUserSession(id, false)
     revalidatePath('/')
     revalidatePath('/images')
-    return NextResponse.json({ id: id }, { status: 200 });
+    const redirectUrl = new URL('/', url.origin); // Redirect to home page
+    return NextResponse.redirect(redirectUrl); // Redirect response
+    // return NextResponse.json({ id: id }, { status: 200 });
   } catch (error) {
     return NextResponse.json(
       { message: (error as Error).message || "Error adding user" },
