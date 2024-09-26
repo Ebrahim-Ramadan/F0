e6f0a007fe86b4be6688aa34ca5822d55dc38c8d
Ebrahim-Ramadan <ramadanebrahim791@gmail.com>
2024-09-26 02:52:20 +0200

    removed console logs from prod

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
     images: {
       dangerouslyAllowSVG: true,
         remotePatterns: [
