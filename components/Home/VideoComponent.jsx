c487d57a6cc758a6228979f4e9b8ef684c1f0e4b
Ebrahim-Ramadan <ramadanebrahim791@gmail.com>
2024-10-22 14:12:37 +0200

    updated the upload images btn on landpage

--- components/Home/VideoComponent.jsx
+++ components/Home/VideoComponent.jsx
@@ -53,12 +53,13 @@ const VideoComponent = ({loggedIN}) => {
               remove your background within milliseconds.
               </p>
               <button className="mt-2 md:mt-4 flex justify-end w-full">
-                <Link href={loggedIN?`/images`:'/join'} className='shadow-sm shadow-white/50 px-5 py-2 md:px-8 md:py-3 bg-primary-950 text-lg md:text-xl text-black font-semibold rounded-3xl mt-4 hover:bg-primary-900 transition-all duration-300'>
+                <Link href={loggedIN?`/images`:'/join'} className='group relative inline-flex h-11 animate-rainbow cursor-pointer items-center justify-center rounded-xl border-0 bg-[length:200%] px-8 py-2 font-bold text-black  transition-colors [background-clip:padding-box,border-box,border-box] [background-origin:border-box] [border:calc(0.08*1rem)_solid_transparent] focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 before:absolute before:bottom-[-20%] before:left-1/2 before:z-0 before:h-1/5 before:w-3/5 before:-translate-x-1/2 before:animate-rainbow before:bg-[linear-gradient(90deg,hsl(var(--color-1)),hsl(var(--color-5)),hsl(var(--color-3)),hsl(var(--color-4)),hsl(var(--color-2)))] before:bg-[length:200%] before:[filter:blur(calc(0.8*1rem))] bg-[linear-gradient(#121213,#121213),linear-gradient(#121213_50%,rgba(18,18,19,0.6)_80%,rgba(18,18,19,0)),linear-gradient(90deg,hsl(var(--color-1)),hsl(var(--color-5)),hsl(var(--color-3)),hsl(var(--color-4)),hsl(var(--color-2)))] dark:bg-[linear-gradient(#fff,#fff),linear-gradient(#fff_50%,rgba(255,255,255,0.6)_80%,rgba(0,0,0,0)),linear-gradient(90deg,hsl(var(--color-1)),hsl(var(--color-5)),hsl(var(--color-3)),hsl(var(--color-4)),hsl(var(--color-2)))]'>
                   {loggedIN ?
                 'Upload Images':
                 'Sign Up'  
                 }
                   </Link>
+
               </button>
             </div>
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,7 @@ const config: Config = {
   theme: {
     extend: {
       animation: {
+        rainbow: "rainbow var(--speed, 2s) infinite linear",
         'spin-slow': 'spin 0.2s linear infinite',
 
         fadeIn: 'fadeIn 0.3s ease-out',
@@ -16,6 +17,10 @@ const config: Config = {
         'float-move': 'float-move 1s ease-out forwards',
       },
       keyframes: {
+        rainbow: {
+          "0%": { "background-position": "0%" },
+          "100%": { "background-position": "200%" },
+        },
         fadeIn: {
           '0%': { opacity: '0' },
           '100%': { opacity: '1' },
@@ -33,6 +38,11 @@ const config: Config = {
         },
       },
       colors: {
+        "color-1": "hsl(var(--color-1))",
+        "color-2": "hsl(var(--color-2))",
+        "color-3": "hsl(var(--color-3))",
+        "color-4": "hsl(var(--color-4))",
+        "color-5": "hsl(var(--color-5))",
         primary: {
           50:  '#000000', // Lightest shade
           100: '#121212', // Lightest shade
